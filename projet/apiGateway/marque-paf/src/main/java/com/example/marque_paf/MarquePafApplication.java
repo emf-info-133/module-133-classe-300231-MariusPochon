fe64236145627337bfa5b070cd0c39045bfe0ba9
Siwan1680 <siwan.khalil@studentfr.ch>
2025-04-07 13:40:36 +0200

    dockerfile --> apiGateway

---
+++ projet/apiGateway/marque-paf/src/main/java/com/example/marque_paf/MarquePafApplication.java
@@ -0,0 +1,13 @@
+package com.example.marque_paf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarquePafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarquePafApplication.class, args);
+	}
+
+}
---
+++ projet/apiGateway/marque-paf/src/test/java/com/example/marque_paf/MarquePafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marque_paf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarquePafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
